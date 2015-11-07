8c4615c465d6526767f77935ec8038b7b196b458
Sacharc <m@m>
2015-11-07 17:33:46 +0100

    Interfejs modelu

--- src/AEW/model/Model.java
+++ src/AEW/model/Model.java
@@ -23,5 +23,33 @@ public class Model
 	{
 		this.plansza = new Plansza(gracz1,gracz2);
 	}
+	
+	/**
+	 * Probuje wykonac ruch
+	 * @param x
+	 * @param y
+	 */
+	public void ruch(int x, int y) {
+	    //TODO
+	}
+	
+	/**
+	 * Sprawdza czy pionek moze zostac ruszony
+	 * Jezeli gracz ma bicie to musi je wykonac - nie moze ruszyc pionkiem nie majacym bicia
+	 * @return true jezeli pionek moze zostac ruszony
+	 */
+	public boolean czyMaRuch() {
+	    //TODO
+	    return true;
+	}
+	
+	/**
+	 * Sprawdza czy lista bic nie jest pusta
+	 * @return
+	 */
+	private boolean czySaBicia() {
+	    //TODO
+        return false;
+	}
 
 }
