e8e884b4b8191bcf6b76f954757363237efd854f
Sacharc <m@m>
2015-11-28 14:52:53 +0100

    Pakiet na losowego gracza

---
+++ src/AEW/ai/losowy/KomputerLosowy.java
@@ -0,0 +1,13 @@
+package AEW.ai.losowy;
+
+import AEW.ai.Komputer;
+
+public class KomputerLosowy extends Komputer{
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
